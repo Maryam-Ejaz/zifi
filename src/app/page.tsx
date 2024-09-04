0b99e1a9d94c2652535695ac80ff574a04b8c265
Mariam Ejaz <81904965+Maryam-Ejaz@users.noreply.github.com>
2024-09-04 21:45:40 +0500

    Added some data again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import dynamic from 'next/dynamic';
 const MainSpeedPage = dynamic(() => import('./pages/MainSpeedPage'), { ssr: false });
 import { ThemeProvider } from "./theme/ThemeProvider";
-import SpeedCheckerComponent from './components/SpeedCheckerComponent';
 import { LocationProvider } from './contexts/LocationContext';
 
 export default function Home() {
   return (
     <ThemeProvider>
       <LocationProvider>
-      {/* <SpeedCheckerComponent/> */}
       <MainSpeedPage/>
       </LocationProvider>
     </ThemeProvider>
